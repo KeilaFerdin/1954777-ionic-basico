fab039a8f738612a307ab71823fd0b1e3558863e
keila monserrat ferdin hernandez <166959808+KeilaFerdin@users.noreply.github.com>
2024-10-13 18:23:46 +0000

    Practica 6 proyecto Ionic en la carpeta miApp-1954777

---
+++ miApp-1954777/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miApp-1954777',
+  webDir: 'www'
+};
+
+export default config;

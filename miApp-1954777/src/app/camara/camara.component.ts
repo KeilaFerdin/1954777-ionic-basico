e765e2cfadfbac2bd1c082a954e1a55c79f42c9c
KeilaFerdin <ferinkeila@gmail.com>
2024-10-27 00:11:19 -0600

    Practica 12: Acceso a Recursos - Parte 2

--- miApp-1954777/src/app/camara/camara.component.ts
+++ miApp-1954777/src/app/camara/camara.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FotoserviceService } from '../foto-service.service'; 
 import { IonicModule } from '@ionic/angular'; 
+import { Foto } from '../foto.model'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-camara',
   templateUrl: './camara.component.html',
   standalone: true, 
   styleUrls: ['./camara.component.scss'],
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class CamaraComponent implements OnInit {
   
+  public fotos: Foto[] = [];
+
   constructor(private fotoService: FotoserviceService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fotos = this.fotoService.fotos;
+  }
 
   tomarFoto() {
-    this.fotoService.addNewToGallery(); 
+    this.fotoService.addNewToGallery().then(() => {
+      this.fotos = this.fotoService.fotos;
+    });
   }
 }
--- miApp-1954777/src/app/foto-service.service.ts
+++ miApp-1954777/src/app/foto-service.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
-import { Filesystem, Directory } from '@capacitor/filesystem';
-import { Preferences } from '@capacitor/preferences';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Foto } from './foto.model'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class FotoserviceService {
+  public fotos: Foto[] = []; // Variable para almacenar las fotos
 
   public async addNewToGallery() {
+    // Toma la foto usando la cámara
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
     });
+
+    
+    if (capturedPhoto && capturedPhoto.webPath) {
+   
+      this.fotos.unshift({
+        filepath: 'img_' + new Date().getTime() + '.jpeg', // Nombre único para la imagen
+        webViewPath: capturedPhoto.webPath // Ruta de la imagen
+      });
+    } else {
+      console.error('Error al capturar la foto');
+    }
   }
 }

0232bf5c06f425cea33b912e29a6e00d8cecc318
KeilaFerdin <ferinkeila@gmail.com>
2024-11-03 14:08:51 -0600

    Practica 13: Build de App

--- miApp-1954777/src/app/app.component.ts
+++ miApp-1954777/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,23 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  platform: string;
+
+  constructor() {
+    this.platform = Capacitor.getPlatform();
+    this.detectPlatform();
+  }
+
+  detectPlatform() {
+    if (this.platform === 'android') {
+      console.log('Estás en Android');
+   
+    } else if (this.platform === 'ios') {
+      console.log('Estás en iOS');
+   
+    } else if (this.platform === 'web') {
+      console.log('Estás en Web');
+  
+    }
+  }
 }
